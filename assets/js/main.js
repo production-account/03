21064e6924ea124816ac213516a766992763e69c
production-account <miwaSO@protonmail.com>
2026-09-07 11:15:20 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
+'use strict;'
+{
+    const target = document.querySelector('.read');
 
+    function callback(entries){
+        console.log(entries[0]);
+
+        if(!entries[0].isIntersecting){
+            return;
+        }
+        entries[0].target.classList.add('appear');
+
+        //     entries[0].target.classList.add('appear');
+        // } else {
+        //     entries[0].target.classList.remove('appear')
+    }
+
+    const options = {
+        threshold: 0.2,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(target);
+}
